7015474729ce4458014b7a0a0cdc1d7a658c252c
NBProgramm <96227838+NBProgramm@users.noreply.github.com>
2021-12-16 09:03:20 +0100

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,9 +1,8 @@
-grösse=(input("Deine Grösse ist(m):")) #Gib ein wie gross du bist
-gewicht=(input("Dein Gewicht ist:(kg gerundet)")) # Gibi ein wie schwer du bist
+grösse=(input("Deine Grösse ist(m):")) # Der Computer fragt sie  wie gross sie sind
+gewicht=(input("Dein Gewicht ist:(kg gerundet)")) # Der Computer fragt sie wie  Schwer sie sind
 
 
 
-grösse2=float(grösse)*float(grösse) #rechnet grösse mal grösse
-
-bmi=int(gewicht)/float(grösse2) # Es rechnet das gewicht mal die grösse^2
+grösse2=float(grösse)*float(grösse) # Das Programm rechnet die grösse^2
+bmi=int(gewicht)/float(grösse2) # Der Computer rechnet das gewicht mal die grösse^2. Das ist die vormel für das berechnen des BMI
 print("Dein BMI ist:  " +str(bmi)) # Ausgabe 
